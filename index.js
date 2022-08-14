b8081c65d28a75fe56d9e63e7261811dc8efbbf2
Hawala-Steven <110100717+Hawala-Steven@users.noreply.github.com>
2022-08-15 02:35:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+let random = Math.random(1) * 10;
+let num3 = 16;
+let num4 = 4;
+const mod = num3 / num4;
+const max = Math.max(16, 17, 18, 19, 20);
 
 // Write your code here
